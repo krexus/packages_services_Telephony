130e1b1935f477ec99255de2faf3d08803fd6c3b
George G <kreach3r@users.noreply.github.com>
2015-12-11 02:50:29 +0200

    National Roaming: Upgrade logic (1/2)
    Adapted for Nougat

--- src/com/android/phone/MobileNetworkSettings.java
+++ src/com/android/phone/MobileNetworkSettings.java
@@ -505,7 +505,7 @@ public class MobileNetworkSettings extends PreferenceActivity
         mButtonDataRoam.setChecked(mPhone.getDataRoamingEnabled());
         mButtonNationalDataRoam.setChecked(android.provider.Settings.System.getInt(
                 mPhone.getContext().getContentResolver(),
-                android.provider.Settings.System.MVNO_ROAMING, 0) == 1);
+                android.provider.Settings.System.MVNO_ROAMING, 1) == 1);
 
         if (getPreferenceScreen().findPreference(BUTTON_PREFERED_NETWORK_MODE) != null
                 || getPreferenceScreen().findPreference(BUTTON_ENABLED_NETWORKS_KEY) != null)  {
@@ -735,7 +735,7 @@ public class MobileNetworkSettings extends PreferenceActivity
         mButtonDataRoam.setChecked(mPhone.getDataRoamingEnabled());
         mButtonNationalDataRoam.setChecked(android.provider.Settings.System.getInt(
                 mPhone.getContext().getContentResolver(),
-                android.provider.Settings.System.MVNO_ROAMING, 0) == 1);
+                android.provider.Settings.System.MVNO_ROAMING, 1) == 1);
 
         mButtonEnabledNetworks.setValue(Integer.toString(settingsNetworkMode));
         mButtonPreferredNetworkMode.setValue(Integer.toString(settingsNetworkMode));
