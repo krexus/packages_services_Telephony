df37e9cde58267b78da135197a5dc4b3ffb0fa66
George G <kreach3r@users.noreply.github.com>
2016-08-16 19:25:28 +0300

    Get back automatic network selection
    Partial revert of the "Improve network selection" commit.
    The newly introduced logic was removing the whole PreferenceGroup everytime it updated the network selections, and the
    automatic selection was removed.
    
    This commit fixes that.

--- src/com/android/phone/NetworkSetting.java
+++ src/com/android/phone/NetworkSetting.java
@@ -582,7 +582,9 @@ public class NetworkSetting extends PreferenceActivity
     }
 
     private void clearList() {
-        mNetworkList.removeAll();
+        for (Preference p : mNetworkMap.keySet()) {
+            mNetworkList.removePreference(p);
+        }
         mNetworkMap.clear();
     }
 
